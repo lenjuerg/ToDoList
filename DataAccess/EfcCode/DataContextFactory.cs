fbf4e1f1f6cb0aa74b882cf8a6d568baf6e97027
leju <lennartjuergens@gmx.net>
2024-02-09 16:46:26 +0100

    add data context

---
+++ DataAccess/EfcCode/DataContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EfcCode
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var dbContextBuilder = new DbContextOptionsBuilder<DataContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            dbContextBuilder.UseSqlServer(connectionString);
+
+            return new DataContext(dbContextBuilder.Options);
+        }
+    }
+}

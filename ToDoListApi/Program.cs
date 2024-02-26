f183e77a44eec306b6ea4c31aea07c64703ba25f
leju <lennartjuergens@gmx.net>
2024-02-26 09:26:05 +0100

    reinstated factory

--- ToDoListApi/Program.cs
+++ ToDoListApi/Program.cs
@@ -17,13 +17,38 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
+{
     options.MapType<TimeOnly>(() => new OpenApiSchema
     {
         Type = "string",
         Format = "time",
         Example = new OpenApiString("18:00:00")
-    }));
+    });
 
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+    
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
